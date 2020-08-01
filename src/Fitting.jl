6d7125ba36dad4012563527a5cda57c5b698eabc
singularitti <singularitti@outlook.com>
2020-08-01 18:25:50 -0400

    Try to use the method mentioned in #11

--- src/Fitting.jl
+++ src/Fitting.jl
@@ -2,14 +2,23 @@ module Fitting
 
 using ConstructionBase: constructorof
 using LsqFit: curve_fit
+using Setfield
 using Unitful: AbstractQuantity, NoDims, upreferred, ustrip, unit, dimension, @u_str
 
-# using ..Collections: EquationOfStateOfSolids
+using ..Collections
 
 export linfit, nonlinfit
 
-# function nonlinfit(eos::EquationOfStateOfSolids, xs, ys; kwargs...)
-
-# end # function nonlinfit
+function nonlinfit(eos, xs, ys; kwargs...)
+    params = [eos.v0, eos.b0, eos.b′0]
+    function model(x, p)
+        @set! eos.v0 = p[1]
+        @set! eos.b0 = p[2]
+        @set! eos.b′0 = p[3]
+        return eos.(x)
+    end
+    fit = curve_fit(model, xs, ys, params; kwargs...)
+    return fit
+end # function nonlinfit
 
 end
