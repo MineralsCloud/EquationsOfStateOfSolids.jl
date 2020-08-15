36cfc249f06e31950cb8baee742e67b1aa0874ee
singularitti <singularitti@outlook.com>
2020-08-15 02:35:44 -0400

    Deprecate `float_collect`, use `float.`

--- src/Fitting.jl
+++ src/Fitting.jl
@@ -4,40 +4,15 @@ using ConstructionBase: constructorof
 using LsqFit: curve_fit
 using Unitful: AbstractQuantity, NoDims, upreferred, ustrip, unit, dimension, @u_str
 
-using ..Collections: EnergyEoss, PressureEoss, fieldvalues
+using ..Collections: EnergyEoss, PressureEoss
 
 export linfit, nonlinfit
 
-const float_collect = float ∘ collect
-
 function nonlinfit(eos::S, xs, ys; kwargs...) where {T,S<:EnergyEoss{T}}
     constructor = constructorof(S) ∘ constructorof(T)
-    # model(x, p) = map(constructor(p), x)
-    model(x, p) = [constructor(p[1:end-1])(xx, p[end]) for xx in x]
-    fit = curve_fit(
-        model,
-        float_collect(xs),
-        float_collect(ys),
-        vcat(float_collect(fieldvalues(eos.params)), minimum(ys));
-        kwargs...,
-    )
-    if fit.converged
-        return (constructorof(T)(fit.param), fit.param[end]), fit
-    else
-        @error "fitting is not converged! Check your initial parameters!"
-        return nothing, fit
-    end
-end # function nonlinfit
-function nonlinfit(eos::S, xs, ys; kwargs...) where {T,S<:PressureEoss{T}}
-    constructor = constructorof(S) ∘ constructorof(T)
     model(x, p) = map(constructor(p), x)
-    fit = curve_fit(
-        model,
-        float_collect(xs),
-        float_collect(ys),
-        float_collect(fieldvalues(eos.params));
-        kwargs...,
-    )
+    fit =
+        curve_fit(model, float.(xs), float.(ys), float.(fieldvalues(eos.params)); kwargs...)
     if fit.converged
         return constructorof(T)(fit.param), fit
     else
@@ -45,5 +20,8 @@ function nonlinfit(eos::S, xs, ys; kwargs...) where {T,S<:PressureEoss{T}}
         return nothing, fit
     end
 end # function nonlinfit
+
+
+fieldvalues(x) = (getfield(x, i) for i in 1:nfields(x))
 
 end
