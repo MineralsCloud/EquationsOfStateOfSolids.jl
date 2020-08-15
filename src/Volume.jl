7875d963db9bb5d52956c13069ed2bc130bab70a
singularitti <singularitti@outlook.com>
2020-08-15 09:35:20 -0400

    Add module `Volume` & `volumeof`

---
+++ src/Volume.jl
@@ -0,0 +1,13 @@
+module Volume
+
+using UnPack: @unpack
+
+using ..Collections
+
+export volumeof
+
+function volumeof(eos::PressureEoss{<:Murnaghan}, p)
+    @unpack v0, b0, b′0, e0 = eos.param
+    return v0 * (1 + b′0 / b0 * p)^(-1 / b′0)
+end
+end
