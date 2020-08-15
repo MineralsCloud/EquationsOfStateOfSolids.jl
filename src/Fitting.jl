c3b8f7885ddaea2062424d8be6422cfd1b2863a6
singularitti <singularitti@outlook.com>
2020-08-15 02:52:40 -0400

    Separate `_create_model` out of `nonlinfit` as in #32

--- src/Fitting.jl
+++ src/Fitting.jl
@@ -4,13 +4,12 @@ using ConstructionBase: constructorof
 using LsqFit: curve_fit
 using Unitful: AbstractQuantity, NoDims, upreferred, ustrip, unit, dimension, @u_str
 
-using ..Collections: EnergyEoss, PressureEoss
+using ..Collections: EquationOfStateOfSolids
 
 export linfit, nonlinfit
 
-function nonlinfit(eos::S, xs, ys; kwargs...) where {T,S<:EnergyEoss{T}}
-    constructor = constructorof(S) ∘ constructorof(T)
-    model(x, p) = map(constructor(p), x)
+function nonlinfit(eos::EquationOfStateOfSolids{T}, xs, ys; kwargs...) where {T}
+    model = _create_model(eos)
     fit =
         curve_fit(model, float.(xs), float.(ys), float.(fieldvalues(eos.param)); kwargs...)
     if fit.converged
@@ -21,6 +20,10 @@ function nonlinfit(eos::S, xs, ys; kwargs...) where {T,S<:EnergyEoss{T}}
     end
 end # function nonlinfit
 
+function _create_model(::S) where {T,S<:EquationOfStateOfSolids{T}}
+    constructor = constructorof(S) ∘ constructorof(T)
+    return (x, p) -> map(constructor(p), x)
+end
 
 fieldvalues(x) = (getfield(x, i) for i in 1:nfields(x))
 
