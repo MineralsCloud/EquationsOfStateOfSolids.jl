f203ada3e62af3130555d4d9fe3c409d7f477e64
singularitti <singularitti@outlook.com>
2020-08-16 08:56:24 -0400

    Rename `volumeof` to `findvolume`

--- src/Volume.jl
+++ src/Volume.jl
@@ -5,13 +5,19 @@ using UnPack: @unpack
 
 using ..Collections
 
-export volumeof
+export findvolume
 
-function volumeof(eos::PressureEoss{<:Murnaghan}, p)
+function findvolume(eos::PressureEoss{<:Murnaghan}, p)
     @unpack v0, b0, b′0, e0 = eos.param
     return v0 * (1 + b′0 / b0 * p)^(-1 / b′0)
 end
-function volumeof(eos::EnergyEoss{<:BirchMurnaghan3rd}, e, epsilon = 1e-20)
+function findvolume(eos::EnergyEoss{<:BirchMurnaghan2nd}, e)
+    @unpack v0, b0, b′0, e0 = eos.param
+    f = sqrt(2 / 9 * (e - e0) / b0 / v0)
+    vs = map(volume_from_strain(Eulerian(), v0), [f, -f])
+    return map(real, filter(isreal, vs))
+end
+function findvolume(eos::EnergyEoss{<:BirchMurnaghan3rd}, e; epsilon = 1e-20)
     @unpack v0, b0, b′0, e0 = eos.param
     # Constrcut ax^3 + bx^2 + d = 0
     b, d = 9 / 2 * b0 * v0, e0 - e
