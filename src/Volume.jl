a8e6495fcff923f1a6e3de2e42c5bff41b910c0b
singularitti <singularitti@outlook.com>
2020-08-16 09:02:27 -0400

    Move `findvolume` from https://github.com/MineralsCloud/EquationsOfState.jl/blob/7648693f08c36cc0f1d1b520a6381027ada9699a/src/Find.jl#L55-L128 to here

--- src/Volume.jl
+++ src/Volume.jl
@@ -1,6 +1,31 @@
 module Volume
 
 using PolynomialRoots: roots
+using Roots:
+    find_zero,
+    Halley,
+    Schroder,
+    Bisection,
+    BisectionExact,
+    FalsePosition,
+    A42,
+    AlefeldPotraShi,
+    Esser,
+    King,
+    KumarSinghAkanksha,
+    Order0,
+    Order16,
+    Order1B,
+    Order2,
+    Order2B,
+    Order5,
+    Order8,
+    Secant,
+    Steffensen,
+    Thukral16,
+    Thukral8,
+    Brent,
+    Newton
 using UnPack: @unpack
 
 using ..Collections
@@ -27,5 +52,79 @@ function findvolume(eos::EnergyEoss{<:BirchMurnaghan3rd}, e; epsilon = 1e-20)
     vs = map(volume_from_strain(Eulerian(), v0), fs)
     return map(real, filter(isreal, vs))
 end
+function findvolume(eos::EquationOfStateOfSolids, y, v0, method)
+    v = _findvolume(eos, y, v0, method)
+    if v < zero(v)
+        @error "the volume found is negative!"
+    end
+    return v
+end # function findvolume
+function findvolume(eos::EquationOfStateOfSolids, y, v0; silent = false)
+    for T in [
+        Bisection,
+        BisectionExact,
+        FalsePosition,
+        A42,
+        AlefeldPotraShi,
+        Brent,
+        Halley,
+        Schroder,
+        Newton,
+        Esser,
+        King,
+        KumarSinghAkanksha,
+        Order0,
+        Order16,
+        Order1B,
+        Order2,
+        Order2B,
+        Order5,
+        Order8,
+        Secant,
+        Steffensen,
+        Thukral16,
+        Thukral8,
+    ]
+        silent || @info "using method `$T`..."
+        try
+            # `maximum` and `minimum` also works with `AbstractQuantity`s.
+            return findvolume(eos, y, v0, T())
+        catch e
+            silent || @info "method `$T` failed because of $e."
+            continue
+        end
+    end
+end # function findvolume
+_findvolume(
+    eos,
+    y,
+    v0,
+    method::Union{Bisection,BisectionExact,FalsePosition,A42,AlefeldPotraShi},
+) = find_zero(v -> eos(v) - y, (minimum(v0), maximum(v0)), method)
+_findvolume(
+    eos,
+    y,
+    v0,
+    method::Union{
+        Brent,
+        Halley,
+        Schroder,
+        Newton,
+        Esser,
+        King,
+        KumarSinghAkanksha,
+        Order0,
+        Order16,
+        Order1B,
+        Order2,
+        Order2B,
+        Order5,
+        Order8,
+        Secant,
+        Steffensen,
+        Thukral16,
+        Thukral8,
+    },
+) = find_zero(v -> eos(v) - y, (minimum(v0) + maximum(v0)) / 2, method)
 
 end
