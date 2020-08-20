c39103701b344a95d457e5225775d837edec9c83
singularitti <singularitti@outlook.com>
2020-08-20 14:08:32 -0400

    Fix typos in `strain_volume_derivative`s

--- src/Collections.jl
+++ src/Collections.jl
@@ -298,30 +298,30 @@ volume_from_strain(::InfinitesimalStrain, v0) = f -> v0 / (1 - f)^3
 
 function strain_volume_derivative(s::EulerianStrain, v0, v, deg::Integer)
     if deg == 1
-        return -1 / 3 / v * cbrt(v0 / v)^2
+        return -(v0 / v)^(2 / 3) / 3 / v
     else  # Recursion
-        return -(3deg + 2) / 3 / v * strain_volume_derivative(s, v0, v, deg - 1)
+        return -(3deg - 1) / 3 / v * strain_volume_derivative(s, v0, v, deg - 1)
     end
 end
 function strain_volume_derivative(s::LagrangianStrain, v0, v, deg::Integer)
     if deg == 1
-        return -1 / 3 / v * cbrt(v / v0)^2
+        return -(v / v0)^(2 / 3) / 3 / v
     else  # Recursion
-        return -(3deg - 2) / 3 / v * strain_volume_derivative(s, v0, v, deg - 1)
+        return -(3deg - 5) / 3 / v * strain_volume_derivative(s, v0, v, deg - 1)
     end
 end
 function strain_volume_derivative(s::NaturalStrain, v0, v, deg::Integer)
     if deg == 1
         return 1 / 3 / v
     else  # Recursion
-        return -deg / v * strain_volume_derivative(s, v0, v, deg - 1)
+        return -(deg - 1) / v * strain_volume_derivative(s, v0, v, deg - 1)
     end
 end
 function strain_volume_derivative(s::InfinitesimalStrain, v0, v, deg::Integer)
     if deg == 1
-        return (1 - s(v0, v))^4 / 3 / v0
+        return (1 - strain_from_volume(s, v0)(v))^4 / 3 / v0
     else  # Recursion
-        return -(3deg + 1) / 3 / v * strain_volume_derivative(s, v0, v, deg - 1)
+        return -(3deg - 2) / 3 / v * strain_volume_derivative(s, v0, v, deg - 1)
     end
 end
 
