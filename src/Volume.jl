661e2d586c9a71769e8cccea462ceafb8aa49156
singularitti <singularitti@outlook.com>
2020-08-21 16:13:52 -0400

    Rename `epsilon` to `root_thr` in `findvolume`

--- src/Volume.jl
+++ src/Volume.jl
@@ -38,13 +38,13 @@ function findvolume(eos::EnergyEOS{<:BirchMurnaghan2nd}, e)
     vs = map(strain2volume(EulerianStrain(), v0), [f, -f])
     return map(real, filter(isreal, vs))
 end
-function findvolume(eos::EnergyEOS{<:BirchMurnaghan3rd}, e; epsilon = 1e-20)
+function findvolume(eos::EnergyEOS{<:BirchMurnaghan3rd}, e; root_thr = 1e-20)
     @unpack v0, b0, b′0, e0 = eos.param
     # Constrcut ax^3 + bx^2 + d = 0
     b, d = 9 / 2 * b0 * v0, e0 - e
     a = b * (b′0 - 4)
     # Solve ax^3 + bx^2 + d = 0
-    fs = roots([d, 0, b, a]; polish = true, epsilon = epsilon)
+    fs = roots([d, 0, b, a]; polish = true, epsilon = root_thr)
     vs = map(strain2volume(EulerianStrain(), v0), fs)
     return map(real, filter(isreal, vs))
 end
