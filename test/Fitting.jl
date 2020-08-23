1c9564243edc2ff27a06633f110a4332241f368f
singularitti <singularitti@outlook.com>
2020-08-23 14:54:22 -0400

    Remove redundant `e0` options

--- test/Fitting.jl
+++ test/Fitting.jl
@@ -20,7 +20,7 @@ _isapprox(a::T, b::T; kwargs...) where {T<:Parameters} =
     end
     volumes, energies = data["volume"], data["energy"]
     @test _isapprox(
-        nonlinfit(EnergyEOS(BirchMurnaghan3rd(40, 0.5, 4, 0)), volumes, energies),
+        nonlinfit(EnergyEOS(BirchMurnaghan3rd(40, 0.5, 4)), volumes, energies),
         BirchMurnaghan3rd(
             40.98926572528106,
             0.5369258245417454,
@@ -29,7 +29,7 @@ _isapprox(a::T, b::T; kwargs...) where {T<:Parameters} =
         ),
     )
     @test _isapprox(
-        linfit(EnergyEOS(BirchMurnaghan3rd(40, 0.5, 4, 0)), volumes, energies),
+        linfit(EnergyEOS(BirchMurnaghan3rd(40, 0.5, 4)), volumes, energies),
         BirchMurnaghan3rd(
             40.98926572528106,
             0.5369258245417454,
@@ -38,7 +38,7 @@ _isapprox(a::T, b::T; kwargs...) where {T<:Parameters} =
         ),
     )
     @test _isapprox(
-        nonlinfit(EnergyEOS(Murnaghan(41, 0.5, 4, 0)), volumes, energies),
+        nonlinfit(EnergyEOS(Murnaghan(41, 0.5, 4)), volumes, energies),
         Murnaghan(
             41.13757930387086,
             0.5144967693786603,
@@ -47,7 +47,7 @@ _isapprox(a::T, b::T; kwargs...) where {T<:Parameters} =
         ),
     )
     @test _isapprox(
-        nonlinfit(EnergyEOS(PoirierTarantola3rd(41, 0.5, 4, 0)), volumes, energies),
+        nonlinfit(EnergyEOS(PoirierTarantola3rd(41, 0.5, 4)), volumes, energies),
         PoirierTarantola3rd(
             40.86770643373908,
             0.5667729960804602,
@@ -56,7 +56,7 @@ _isapprox(a::T, b::T; kwargs...) where {T<:Parameters} =
         ),
     )
     @test _isapprox(
-        linfit(EnergyEOS(PoirierTarantola3rd(41, 0.5, 4, 0)), volumes, energies),
+        linfit(EnergyEOS(PoirierTarantola3rd(41, 0.5, 4)), volumes, energies),
         PoirierTarantola3rd(
             40.86770643373908,
             0.5667729960804602,
@@ -65,7 +65,7 @@ _isapprox(a::T, b::T; kwargs...) where {T<:Parameters} =
         ),
     )
     @test _isapprox(
-        nonlinfit(EnergyEOS(Vinet(41, 0.5, 4, 0)), volumes, energies),
+        nonlinfit(EnergyEOS(Vinet(41, 0.5, 4)), volumes, energies),
         Vinet(
             40.916875663779784,
             0.5493839425156859,
@@ -80,14 +80,20 @@ end
     data = open("test/data/mp153.yml", "r") do io
         YAML.load(io)
     end
-    volumes, energies, known_energies_vinet =
-        data["volume"], data["energy"], data["known_energy_vinet"]
-    fitted_eos = nonlinfit(EnergyEOS(Vinet(23, 0.5, 4, -2)), volumes, energies)
-    @test _isapprox(
-        fitted_eos,
-        Vinet(22.957645593, 0.22570911414, 4.06054339, -1.59442926062),
-    )
-    @test isapprox(map(EnergyEOS(fitted_eos), volumes), known_energies_vinet; atol = 1e-6)
+    @testset "without unit" begin
+        volumes, energies, known_energies_vinet =
+            data["volume"], data["energy"], data["known_energy_vinet"]
+        fitted_eos = nonlinfit(EnergyEOS(Vinet(23, 0.5, 4)), volumes, energies)
+        @test _isapprox(
+            fitted_eos,
+            Vinet(22.957645593, 0.22570911414, 4.06054339, -1.59442926062),
+        )
+        @test isapprox(
+            map(EnergyEOS(fitted_eos), volumes),
+            known_energies_vinet;
+            atol = 1e-6,
+        )
+    end
 end
 
 # Data from https://github.com/materialsproject/pymatgen/blob/19c4d98/pymatgen/analysis/tests/test_eos.py#L185-L260
