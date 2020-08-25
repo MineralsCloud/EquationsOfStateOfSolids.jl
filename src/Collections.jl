4bad6bf1b022993c629f21755df95bc4e198eb8f
singularitti <singularitti@outlook.com>
2020-08-24 21:45:08 -0400

    Add docs to EOS constructors

--- src/Collections.jl
+++ src/Collections.jl
@@ -53,6 +53,20 @@ abstract type Parameters{T} end
 abstract type FiniteStrainParameters{N,T} <: Parameters{T} end
 abstract type BirchMurnaghan{N,T} <: FiniteStrainParameters{N,T} end
 abstract type PoirierTarantola{N,T} <: FiniteStrainParameters{N,T} end
+"""
+    Murnaghan(v0, b0, b′0, e0=zero(v0 * b0))
+
+Create a Murnaghan equation of state.
+
+This equation of state can have units. The units are specified in
+[`Unitful.jl`](https://github.com/PainterQubits/Unitful.jl)'s `@u_str` style.
+
+# Arguments
+- `v0`: the volume of solid at zero pressure.
+- `b0`: the bulk modulus of solid at zero pressure.
+- `b′0`: the first-order pressure-derivative bulk modulus of solid at zero pressure. "`′`" can be typed by `\\prime<tab>`.
+- `e0`: the energy of solid at zero pressure.
+"""
 @auto_hash_equals struct Murnaghan{T} <: Parameters{T}
     v0::T
     b0::T
@@ -60,12 +74,39 @@ abstract type PoirierTarantola{N,T} <: FiniteStrainParameters{N,T} end
     e0::T
     Murnaghan{T}(v0, b0, b′0, e0 = zero(v0 * b0)) where {T} = new(v0, b0, b′0, e0)
 end
+"""
+    BirchMurnaghan2nd(v0, b0, e0=zero(v0 * b0))
+
+Create a Birch–Murnaghan second order equation of state.
+
+This equation of state can have units. The units are specified in
+[`Unitful.jl`](https://github.com/PainterQubits/Unitful.jl)'s `@u_str` style.
+
+# Arguments
+- `v0`: the volume of solid at zero pressure.
+- `b0`: the bulk modulus of solid at zero pressure.
+- `e0`: the energy of solid at zero pressure.
+"""
 @auto_hash_equals struct BirchMurnaghan2nd{T} <: BirchMurnaghan{2,T}
     v0::T
     b0::T
     e0::T
     BirchMurnaghan2nd{T}(v0, b0, e0 = zero(v0 * b0)) where {T} = new(v0, b0, e0)
 end
+"""
+    BirchMurnaghan3rd(v0, b0, b′0, e0=zero(v0 * b0))
+
+Create a Birch–Murnaghan third order equation of state.
+
+This equation of state can have units. The units are specified in
+[`Unitful.jl`](https://github.com/PainterQubits/Unitful.jl)'s `@u_str` style.
+
+# Arguments
+- `v0`: the volume of solid at zero pressure.
+- `b0`: the bulk modulus of solid at zero pressure.
+- `b′0`: the first-order pressure-derivative bulk modulus of solid at zero pressure. "`′`" can be typed by `\\prime<tab>`.
+- `e0`: the energy of solid at zero pressure.
+"""
 @auto_hash_equals struct BirchMurnaghan3rd{T} <: BirchMurnaghan{3,T}
     v0::T
     b0::T
@@ -73,6 +114,21 @@ end
     e0::T
     BirchMurnaghan3rd{T}(v0, b0, b′0, e0 = zero(v0 * b0)) where {T} = new(v0, b0, b′0, e0)
 end
+"""
+    BirchMurnaghan4th(v0, b0, b′0, b″0, e0=zero(v0 * b0))
+
+Create a Birch–Murnaghan fourth order equation of state.
+
+This equation of state can have units. The units are specified in
+[`Unitful.jl`](https://github.com/PainterQubits/Unitful.jl)'s `@u_str` style.
+
+# Arguments
+- `v0`: the volume of solid at zero pressure.
+- `b0`: the bulk modulus of solid at zero pressure.
+- `b′0`: the first-order pressure-derivative bulk modulus of solid at zero pressure. "`′`" can be typed by `\\prime<tab>`.
+- `b″0`: the second-order pressure-derivative bulk modulus of solid at zero pressure. "`″`" can be typed by `\\pprime<tab>`.
+- `e0`: the energy of solid at zero pressure.
+"""
 @auto_hash_equals struct BirchMurnaghan4th{T} <: BirchMurnaghan{4,T}
     v0::T
     b0::T
@@ -82,12 +138,39 @@ end
     BirchMurnaghan4th{T}(v0, b0, b′0, b″0, e0 = zero(v0 * b0)) where {T} =
         new(v0, b0, b′0, b″0, e0)
 end
+"""
+    PoirierTarantola2nd(v0, b0, e0=zero(v0 * b0))
+
+Create a Poirier–Tarantola second order equation of state.
+
+This equation of state can have units. The units are specified in
+[`Unitful.jl`](https://github.com/PainterQubits/Unitful.jl)'s `@u_str` style.
+
+# Arguments
+- `v0`: the volume of solid at zero pressure.
+- `b0`: the bulk modulus of solid at zero pressure.
+- `e0`: the energy of solid at zero pressure.
+"""
 @auto_hash_equals struct PoirierTarantola2nd{T} <: PoirierTarantola{2,T}
     v0::T
     b0::T
     e0::T
     PoirierTarantola2nd{T}(v0, b0, e0 = zero(v0 * b0)) where {T} = new(v0, b0, e0)
 end
+"""
+    PoirierTarantola3rd(v0, b0, b′0, e0=zero(v0 * b0))
+
+Create a Poirier–Tarantola third order equation of state.
+
+This equation of state can have units. The units are specified in
+[`Unitful.jl`](https://github.com/PainterQubits/Unitful.jl)'s `@u_str` style.
+
+# Arguments
+- `v0`: the volume of solid at zero pressure.
+- `b0`: the bulk modulus of solid at zero pressure.
+- `b′0`: the first-order pressure-derivative bulk modulus of solid at zero pressure. "`′`" can be typed by `\\prime<tab>`.
+- `e0`: the energy of solid at zero pressure.
+"""
 @auto_hash_equals struct PoirierTarantola3rd{T} <: PoirierTarantola{3,T}
     v0::T
     b0::T
@@ -95,6 +178,21 @@ end
     e0::T
     PoirierTarantola3rd{T}(v0, b0, b′0, e0 = zero(v0 * b0)) where {T} = new(v0, b0, b′0, e0)
 end
+"""
+    PoirierTarantola4th(v0, b0, b′0, b″0, e0=zero(v0 * b0))
+
+Create a Poirier–Tarantola fourth order equation of state.
+
+This equation of state can have units. The units are specified in
+[`Unitful.jl`](https://github.com/PainterQubits/Unitful.jl)'s `@u_str` style.
+
+# Arguments
+- `v0`: the volume of solid at zero pressure.
+- `b0`: the bulk modulus of solid at zero pressure.
+- `b′0`: the first-order pressure-derivative bulk modulus of solid at zero pressure. "`′`" can be typed by `\\prime<tab>`.
+- `b″0`: the second-order pressure-derivative bulk modulus of solid at zero pressure. "`″`" can be typed by `\\pprime<tab>`.
+- `e0`: the energy of solid at zero pressure.
+"""
 @auto_hash_equals struct PoirierTarantola4th{T} <: PoirierTarantola{4,T}
     v0::T
     b0::T
@@ -104,6 +202,20 @@ end
     PoirierTarantola4th{T}(v0, b0, b′0, b″0, e0 = zero(v0 * b0)) where {T} =
         new(v0, b0, b′0, b″0, e0)
 end
+"""
+    Vinet(v0, b0, b′0, e0=zero(v0 * b0))
+
+Create a Vinet equation of state.
+
+This equation of state can have units. The units are specified in
+[`Unitful.jl`](https://github.com/PainterQubits/Unitful.jl)'s `@u_str` style.
+
+# Arguments
+- `v0`: the volume of solid at zero pressure.
+- `b0`: the bulk modulus of solid at zero pressure.
+- `b′0`: the first-order pressure-derivative bulk modulus of solid at zero pressure. "`′`" can be typed by `\\prime<tab>`.
+- `e0`: the energy of solid at zero pressure.
+"""
 @auto_hash_equals struct Vinet{T} <: Parameters{T}
     v0::T
     b0::T
