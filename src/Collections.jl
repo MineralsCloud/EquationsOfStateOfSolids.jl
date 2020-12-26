c36971027d30ce24f0cb62ab70049d1ff6636d62
singularitti <singularitti@outlook.com>
2020-12-26 08:29:16 -0500

    Move `*Strain`s to src/FiniteStrains.jl

--- src/Collections.jl
+++ src/Collections.jl
@@ -12,18 +12,11 @@ export Murnaghan,
     Vinet,
     AntonSchmidt,
     Holzapfel,
-    EulerianStrain,
-    LagrangianStrain,
-    NaturalStrain,
-    InfinitesimalStrain,
     EnergyEquation,
     PressureEquation,
     BulkModulusEquation,
     orderof,
     atomic_number,
-    volume2strain,
-    strain2volume,
-    straintype,
     getparam
 
 include("types.jl")
@@ -48,122 +41,6 @@ orderof(x::FiniteStrainParameters) = orderof(typeof(x))
 
 atomic_number(::Type{<:Holzapfel{Z}}) where {Z} = Z
 atomic_number(x::Holzapfel) = atomic_number(typeof(x))
-
-abstract type FiniteStrain end  # Trait
-struct EulerianStrain <: FiniteStrain end
-struct LagrangianStrain <: FiniteStrain end
-struct NaturalStrain <: FiniteStrain end
-struct InfinitesimalStrain <: FiniteStrain end
-
-"""
-    volume2strain(::EulerianStrain, v0)
-    volume2strain(::LagrangianStrain, v0)
-    volume2strain(::NaturalStrain, v0)
-    volume2strain(::InfinitesimalStrain, v0)
-
-Return a function of `v` that calculates the `FiniteStrain` from `v0`.
-
-!!! info
-    See the formulae on Ref. 1 Table 3.
-"""
-volume2strain(::EulerianStrain, v0) = v -> ((v0 / v)^_⅔ - 1) / 2
-volume2strain(::LagrangianStrain, v0) = v -> ((v / v0)^_⅔ - 1) / 2
-volume2strain(::NaturalStrain, v0) = v -> log(v / v0) / 3
-volume2strain(::InfinitesimalStrain, v0) = v -> 1 - (v0 / v)^_⅓
-
-"""
-    strain2volume(::EulerianStrain, v0)
-    strain2volume(::LagrangianStrain, v0)
-    strain2volume(::NaturalStrain, v0)
-    strain2volume(::InfinitesimalStrain, v0)
-
-Return a function of `f` that calculates the corresponding volume from `v0`.
-
-!!! info
-    See the formulae on Ref. 1 Table 3.
-"""
-function strain2volume(::EulerianStrain, v0)
-    return function (f)
-        if isreal(f)
-            f = real(f)
-        else
-            throw(DomainError("strain $f is complex!"))
-        end
-        if f < -1 / 2
-            throw(DomainError("strain $f < -0.5! Volume will be complex!"))
-        else
-            return v0 / (2f + 1)^_1½
-        end
-    end
-end
-function strain2volume(::LagrangianStrain, v0)
-    return function (f)
-        if isreal(f)
-            f = real(f)
-        else
-            throw(DomainError("strain $f is complex!"))
-        end
-        if f < -1 / 2
-            throw(DomainError("strain $f < -0.5! Volume will be complex!"))
-        else
-            return v0 * (2f + 1)^_1½
-        end
-    end
-end
-strain2volume(::NaturalStrain, v0) = f -> v0 * exp(3f)
-strain2volume(::InfinitesimalStrain, v0) = f -> v0 / (1 - f)^3
-
-"""
-    Dⁿᵥf(s::EulerianStrain, deg, v0)
-    Dⁿᵥf(s::LagrangianStrain, deg, v0)
-    Dⁿᵥf(s::NaturalStrain, deg, v0)
-    Dⁿᵥf(s::InfinitesimalStrain, deg, v0)
-
-Return a function of `v` that calculates the `deg`th order derivative of strain wrt volume from `v0`.
-
-!!! info
-    See the formulae on Ref. 1 Table 3.
-"""
-function Dⁿᵥf(s::EulerianStrain, deg, v0)
-    function (v)
-        if isone(deg)  # Stop recursion
-            return -(v0 / v)^_⅔ / 3 / v
-        else  # Recursion
-            return -(3deg - 1) / 3 / v * Dⁿᵥf(s, deg - 1, v0)(v)
-        end
-    end
-end
-function Dⁿᵥf(s::LagrangianStrain, deg, v0)
-    function (v)
-        if isone(deg)  # Stop recursion
-            return -(v / v0)^_⅔ / 3 / v
-        else  # Recursion
-            return -(3deg - 5) / 3 / v * Dⁿᵥf(s, deg - 1, v0)(v)
-        end
-    end
-end
-function Dⁿᵥf(s::NaturalStrain, deg, v0)
-    function (v)
-        if isone(deg)  # Stop recursion
-            return 1 / 3 / v
-        else  # Recursion
-            return -(deg - 1) / v * Dⁿᵥf(s, deg - 1, v0)(v)
-        end
-    end
-end
-function Dⁿᵥf(s::InfinitesimalStrain, deg, v0)
-    function (v)
-        if isone(deg)  # Stop recursion
-            return (1 - volume2strain(s, v0)(v))^4 / 3 / v0
-        else  # Recursion
-            return -(3deg - 2) / 3 / v * Dⁿᵥf(s, deg - 1, v0)(v)
-        end
-    end
-end
-
-straintype(::Type{<:BirchMurnaghan}) = EulerianStrain
-straintype(::Type{<:PoirierTarantola}) = NaturalStrain
-straintype(x::FiniteStrainParameters) = straintype(typeof(x))
 
 function Base.show(io::IO, param::Parameters)  # Ref: https://github.com/mauro3/Parameters.jl/blob/3c1d72b/src/Parameters.jl#L542-L549
     if get(io, :compact, false)
--- src/EquationsOfStateOfSolids.jl
+++ src/EquationsOfStateOfSolids.jl
@@ -3,6 +3,7 @@ module EquationsOfStateOfSolids
 _ispositive(x) = x > zero(x)  # Do not export!
 
 include("collections.jl")
+include("FiniteStrains.jl")
 include("Fitting.jl")
 include("Volume.jl")
 
