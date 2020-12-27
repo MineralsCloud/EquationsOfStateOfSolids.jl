79d0f59eb9c4f51c970cec1bd24296ad7819f2e6
singularitti <singularitti@outlook.com>
2020-12-27 07:16:32 -0500

    Move the position of `Power`

--- src/types.jl
+++ src/types.jl
@@ -231,24 +231,3 @@ end
 (x::PressureFrom{<:PressureEquation})(v) = x.eos(v)
 (x::BulkModulusFrom)(v) = BulkModulusEquation(getparam(x.eos))(v)
 (x::BulkModulusFrom{<:BulkModulusEquation})(v) = x.eos(v)
-
-# See https://discourse.julialang.org/t/is-there-a-way-to-include-in-function-name/45378/3
-abstract type Power end
-struct TwoThirds <: Power end
-struct OneThird <: Power end
-struct FiveHalves <: Power end
-struct ThreeHalves <: Power end
-
-const _⅔ = TwoThirds()
-const _⅓ = OneThird()
-const _2½ = FiveHalves()
-const _1½ = ThreeHalves()
-
-Base.:(^)(x, ::TwoThirds) = x^(2 // 3)
-Base.:(^)(x::Union{Real,Complex,AbstractQuantity}, ::TwoThirds) = x^(2 / 3)
-Base.:(^)(x, ::OneThird) = x^(1 // 3)
-Base.:(^)(x::Union{Real,Complex,AbstractQuantity}, ::OneThird) = x^(1 / 3)
-Base.:(^)(x, ::FiveHalves) = x^(5 // 2)
-Base.:(^)(x::Union{Real,Complex,AbstractQuantity}, ::FiveHalves) = sqrt(x^5)
-Base.:(^)(x, ::ThreeHalves) = x^(3 // 2)
-Base.:(^)(x::Union{Real,Complex,AbstractQuantity}, ::ThreeHalves) = sqrt(x^3)
