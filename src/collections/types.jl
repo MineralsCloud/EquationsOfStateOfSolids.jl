73ad3048f930c5a26b7fb6588e4b3bed783f0d59
Reno <25192197+singularitti@users.noreply.github.com>
2021-05-29 04:18:30 -0400

    Add docs to `Murnaghan2nd`

--- src/collections/types.jl
+++ src/collections/types.jl
@@ -25,6 +25,21 @@ This equation of state can have units. The units are specified in
     e0::T
     Murnaghan1st{T}(v0, b0, b′0, e0 = zero(v0 * b0)) where {T} = new(v0, b0, b′0, e0)
 end
+"""
+    Murnaghan2nd(v0, b0, b′0, b″0, e0=zero(v0 * b0))
+
+Create a Murnaghan second order equation of state.
+
+This equation of state can have units. The units are specified in
+[`Unitful.jl`](https://github.com/PainterQubits/Unitful.jl)'s `@u_str` style.
+
+# Arguments
+- `v0`: the volume of solid at zero pressure.
+- `b0`: the bulk modulus of solid at zero pressure.
+- `b′0`: the first-order pressure-derivative bulk modulus of solid at zero pressure.
+- `b″0`: the second-order pressure-derivative bulk modulus of solid at zero pressure.
+- `e0`: the energy of solid at zero pressure.
+"""
 @auto_hash_equals struct Murnaghan2nd{T} <: Murnaghan{T}
     v0::T
     b0::T
