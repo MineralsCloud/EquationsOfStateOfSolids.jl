898ec881e986e17c2135aa6c078cd6626bdc4fec
Reno <25192197+singularitti@users.noreply.github.com>
2021-06-11 05:46:56 -0400

    fix: b′0 can't be 4 in `BirchMurnaghan3rd`

--- src/Inverse.jl
+++ src/Inverse.jl
@@ -90,27 +90,31 @@ end
 function (x::AnalyticallyInverted{<:EnergyEquation{<:BirchMurnaghan3rd}})(e)
     @unpack v0, b0, b′0, e0 = getparam(x.eos)
     # Constrcut ax^3 + bx^2 + d = 0, see https://zh.wikipedia.org/wiki/%E4%B8%89%E6%AC%A1%E6%96%B9%E7%A8%8B#%E6%B1%82%E6%A0%B9%E5%85%AC%E5%BC%8F%E6%B3%95
-    a = b′0 - 4
-    r = 1 / 3a  # b = 1
-    d = (e0 - e) / (9 / 2 * b0 * v0)
-    p, q = -r^3 - d / 2a, -r^2
-    Δ = p^2 + q^3
-    fs = -r .+ if Δ > 0
-        (cbrt(p + √Δ) + cbrt(p - √Δ),)  # Only 1 real solution
-    elseif Δ < 0
-        SIN, COS = sincos(acos(p / abs(r)^3) / 3)
-        (2COS, -COS - √3 * SIN, -COS + √3 * SIN) .* abs(r)  # Verified
-    elseif Δ == 0
-        if p == q == 0
-            (0,)  # 3 reals are equal
-        else  # p == -q != 0
-            2cbrt(p), -cbrt(p)  # 2 real roots are equal, leaving 2 solutions
-        end
+    if b′0 == 4
+        throw(DivideError())  # `a` will be zero.
     else
-        @assert false "Δ == p^2 + q^3 == $Δ. this should never happen!"
-    end  # solutions are strains
-    vs = map(FromEulerianStrain(v0), fs)
-    return filter(_ispositive, map(real, filter(isreal, vs)))
+        a = b′0 - 4
+        r = 1 / 3a  # b = 1
+        d = (e0 - e) / (9 / 2 * b0 * v0)
+        p, q = -r^3 - d / 2a, -r^2
+        Δ = p^2 + q^3
+        fs = -r .+ if Δ > 0
+            (cbrt(p + √Δ) + cbrt(p - √Δ),)  # Only 1 real solution
+        elseif Δ < 0
+            SIN, COS = sincos(acos(p / abs(r)^3) / 3)
+            (2COS, -COS - √3 * SIN, -COS + √3 * SIN) .* abs(r)  # Verified
+        elseif Δ == 0
+            if p == q == 0
+                (0,)  # 3 reals are equal
+            else  # p == -q != 0
+                2cbrt(p), -cbrt(p)  # 2 real roots are equal, leaving 2 solutions
+            end
+        else
+            @assert false "Δ == p^2 + q^3 == $Δ. this should never happen!"
+        end  # solutions are strains
+        vs = map(FromEulerianStrain(v0), fs)
+        return filter(_ispositive, map(real, filter(isreal, vs)))
+    end
 end
 function (x::AnalyticallyInverted{<:EnergyEquation{<:PoirierTarantola2nd}})(e)
     @unpack v0, b0, e0 = getparam(x.eos)
