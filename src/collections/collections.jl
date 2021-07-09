c808fb64f45ab5809d4b3af1679082f3b18dfe30
Reno <25192197+singularitti@users.noreply.github.com>
2021-07-09 03:51:16 -0400

    Fix doc test for `orderof`

--- src/collections/collections.jl
+++ src/collections/collections.jl
@@ -21,7 +21,7 @@ Return the order of a `FiniteStrainParameters`.
 
 # Examples
 ```jldoctest
-julia> orderof(BirchMurnaghan3rd(40, 0.5, 4, 0)) == 3
+julia> orderof(BirchMurnaghan(40, 0.5, 4, 0)) == 3
 true
 ```
 """
